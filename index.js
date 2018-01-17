a4cc9028284c388d980237516659223e0e5cd42f
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-01-17 09:22:35 -0800

    fixed bug

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   res.status(404).json({error: {message: 'Not found'}})
 })
 
-app.listen(() => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
 
